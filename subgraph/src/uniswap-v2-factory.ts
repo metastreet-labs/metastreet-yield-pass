0aa47f220f58dab40965afa8858e5ec886ef9f14
Vanya A. Sergeev <v@sergeev.io>
2025-01-09 00:47:01 -0600

    subgraph: add erc20 and erc721 token info to schema

--- subgraph/src/uniswap-v2-factory.ts
+++ subgraph/src/uniswap-v2-factory.ts
@@ -1,17 +1,17 @@
 import { PairCreated as PairCreatedEvent } from "../generated/UniswapV2Factory/UniswapV2Factory";
-import { UniswapV2Pair as UniswapV2PairEntity, YieldPassToken as YieldPassTokenEntity } from "../generated/schema";
+import { UniswapV2Pair as UniswapV2PairEntity, YieldPass as YieldPassEntity } from "../generated/schema";
 
 export function handlePairCreated(event: PairCreatedEvent): void {
-  let yieldPassTokenEntity = YieldPassTokenEntity.load(event.params.token0);
-  if (!yieldPassTokenEntity) yieldPassTokenEntity = YieldPassTokenEntity.load(event.params.token1);
-  if (!yieldPassTokenEntity) return;
+  let yieldPassEntity = YieldPassEntity.load(event.params.token0);
+  if (!yieldPassEntity) yieldPassEntity = YieldPassEntity.load(event.params.token1);
+  if (!yieldPassEntity) return;
 
-  yieldPassTokenEntity.pair = event.params.pair;
-  yieldPassTokenEntity.save();
+  yieldPassEntity.pair = event.params.pair;
+  yieldPassEntity.save();
 
   const pairEntity = new UniswapV2PairEntity(event.params.pair);
   pairEntity.token0 = event.params.token0;
   pairEntity.token1 = event.params.token1;
-  pairEntity.yieldPassToken = yieldPassTokenEntity.id;
+  pairEntity.yieldPass = yieldPassEntity.id;
   pairEntity.save();
 }
--- subgraph/src/yield-pass.ts
+++ subgraph/src/yield-pass.ts
@@ -1,5 +1,6 @@
 import { Address, dataSource } from "@graphprotocol/graph-ts";
 import { ERC20 as ERC20Contract } from "../generated/YieldPass/ERC20";
+import { ERC721 as ERC721Contract } from "../generated/YieldPass/ERC721";
 import { YieldAdapter as YieldAdapterContract } from "../generated/YieldPass/YieldAdapter";
 import {
   Claimed as ClaimedEvent,
@@ -12,46 +13,66 @@ import {
 } from "../generated/YieldPass/YieldPass";
 import {
   ERC20 as ERC20Entity,
+  ERC721 as ERC721Entity,
   YieldAdapter as YieldAdapterEntity,
-  YieldPassToken as YieldPassTokenEntity,
+  YieldPass as YieldPassEntity,
 } from "../generated/schema";
 
 const yieldPassContract = YieldPassContract.bind(dataSource.address());
 
-function createAdapterEntity(yieldPassToken: Address, adapter: Address): void {
-  const adapterContract = YieldAdapterContract.bind(adapter);
+function createERC20Entity(token: Address): ERC20Entity {
+  let erc20Entity = ERC20Entity.load(token);
+  if (erc20Entity) return erc20Entity;
 
-  const erc20Address = adapterContract.token();
-
-  const adapterEntity = new YieldAdapterEntity(adapter);
-  adapterEntity.yieldPassToken = yieldPassToken;
-  adapterEntity.token = erc20Address;
-  adapterEntity.name = adapterContract.name();
-  adapterEntity.save();
-
-  const erc20Contract = ERC20Contract.bind(erc20Address);
+  const erc20Contract = ERC20Contract.bind(token);
   const tokenName = erc20Contract.try_name();
   const tokenSymbol = erc20Contract.try_symbol();
   const tokenDecimals = erc20Contract.try_decimals();
 
-  let erc20Entity = ERC20Entity.load(erc20Address);
-  if (!erc20Entity) {
-    erc20Entity = new ERC20Entity(erc20Address);
-    erc20Entity.name = tokenName.reverted ? "Unnamed Token" : tokenName.value;
-    erc20Entity.symbol = tokenSymbol.reverted ? "UNK" : tokenSymbol.value;
-    erc20Entity.decimals = tokenDecimals.reverted ? 18 : tokenDecimals.value;
-    erc20Entity.save();
-  }
+  erc20Entity = new ERC20Entity(token);
+  erc20Entity.name = tokenName.reverted ? "Unnamed Token" : tokenName.value;
+  erc20Entity.symbol = tokenSymbol.reverted ? "UNK" : tokenSymbol.value;
+  erc20Entity.decimals = tokenDecimals.reverted ? 18 : tokenDecimals.value;
+  erc20Entity.save();
+
+  return erc20Entity;
+}
+
+function createERC721Entity(token: Address): ERC721Entity {
+  let erc721Entity = ERC721Entity.load(token);
+  if (erc721Entity) return erc721Entity;
+
+  const erc721Contract = ERC721Contract.bind(token);
+  const tokenName = erc721Contract.try_name();
+  const tokenSymbol = erc721Contract.try_symbol();
+
+  erc721Entity = new ERC721Entity(token);
+  erc721Entity.name = tokenName.reverted ? "Unnamed Token" : tokenName.value;
+  erc721Entity.symbol = tokenSymbol.reverted ? "UNK" : tokenSymbol.value;
+  erc721Entity.save();
+
+  return erc721Entity;
+}
+
+function createAdapterEntity(yieldPass: Address, adapter: Address): void {
+  const adapterContract = YieldAdapterContract.bind(adapter);
+
+  const adapterEntity = new YieldAdapterEntity(adapter);
+  adapterEntity.name = adapterContract.name();
+  adapterEntity.yieldToken = createERC20Entity(adapterContract.token()).id;
+  adapterEntity.yieldPass = yieldPass;
+  adapterEntity.save();
 }
 
 export function handleYieldPassDeployed(event: YieldPassDeployedEvent): void {
-  const yieldPassTokenEntity = new YieldPassTokenEntity(event.params.yieldPass);
-  yieldPassTokenEntity.startTime = event.params.startTime;
-  yieldPassTokenEntity.expiryTime = event.params.expiryTime;
-  yieldPassTokenEntity.token = event.params.token;
-  yieldPassTokenEntity.nodePass = event.params.nodePass;
-  yieldPassTokenEntity.adapter = event.params.yieldAdapter;
-  yieldPassTokenEntity.save();
+  const yieldPassEntity = new YieldPassEntity(event.params.yieldPass);
+  yieldPassEntity.yieldPassToken = createERC20Entity(event.params.yieldPass).id;
+  yieldPassEntity.nodePassToken = createERC721Entity(event.params.nodePass).id;
+  yieldPassEntity.nftToken = createERC721Entity(event.params.token).id;
+  yieldPassEntity.startTime = event.params.startTime;
+  yieldPassEntity.expiryTime = event.params.expiryTime;
+  yieldPassEntity.adapter = event.params.yieldAdapter;
+  yieldPassEntity.save();
 
   createAdapterEntity(event.params.yieldPass, event.params.yieldAdapter);
 }
